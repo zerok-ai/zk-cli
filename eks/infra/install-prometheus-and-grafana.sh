d6f1efe9df56b68800951de78b602416db654d81
Mudit Krishna Mathur <muditkmathur@gmail.com>
2022-05-17 18:59:06 +0530

    Added better helm charts for prometheus changed the default values for the pods and nodes

--- eks/infra/install-prometheus-and-grafana.sh
+++ eks/infra/install-prometheus-and-grafana.sh
@@ -1,26 +1,7 @@
-
+# This will install prometheus and grafana in monitoring namespace
 # install Prometheus 
-kubectl create namespace prometheus
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install prometheus prometheus-community/prometheus --namespace prometheus
-
-
-# install grafana
-#-------------------------------------------
-kubectl create namespace grafana
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-helm install grafana grafana/grafana  \
-	–namespace grafana   \
-	–set persistence.enabled=true  \
-	–set adminPassword='1rmmIM7hxTa0' \
-	–set datasources.'datasources\.yaml'.apiVersion=1   \
-	–set datasources.'datasources\.yaml'.datasources[0].name=Prometheus  \
-	–set datasources.'datasources\.yaml'.datasources[0].type=prometheus    \
-	–set datasources.'datasources\.yaml'.datasources[0].url=http://prometheus-server.prometheus.svc.cluster.local     \
-	–set datasources.'datasources\.yaml'.datasources[0].access=proxy     \
-	–set datasources.'datasources\.yaml'.datasources[0].isDefault=true 
+kubectl create -f ./manifests/setup/
+kubectl create -f ./manifests/
 
 # validate
 echo 'You can validate the cluster by running -- `kubectl get pods -n prometheus`'
