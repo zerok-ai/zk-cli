5cbb8b398ff3c1d062d1077e7eeac6c8c436fce0
Mudit Krishna Mathur <muditkmathur@gmail.com>
2022-05-21 14:33:13 +0530

    Made service monitor a part of app installation Updated Readme

--- eks/application/install.sh
+++ eks/application/install.sh
@@ -9,6 +9,7 @@ kubectl apply -f deployment.yaml
 kubectl apply -f services.yaml
 kubectl apply -f autoscaler.yaml
 kubectl apply -f ingress.yaml
+kubectl apply -f service-monitor.yaml
 
 echo
 echo "Application deployed at:"
