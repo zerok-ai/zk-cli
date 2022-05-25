76b713bb08b01cb2a112efe0a29f5ef6c0141525
Mudit Krishna Mathur <muditkmathur@gmail.com>
2022-05-25 21:13:07 +0530

    1) default machine to t3.medium 2) zerok scripts

--- eks/application/high-load/zerok-install.sh
+++ eks/application/high-load/zerok-install.sh
@@ -4,6 +4,7 @@ if [[ $1 == "full" ]]; then
 	kubectl apply -f highload-deployment.yaml
 	kubectl apply -f highload-services.yaml
 	kubectl apply -f highload-autoscaler.yaml
+	kubectl apply -f highload-service-monitor.yaml
 fi
 
 kubectl apply -f highload-ingress.yaml
--- eks/application/high-load/zerok-uninstall.sh
+++ eks/application/high-load/zerok-uninstall.sh
@@ -6,4 +6,5 @@ if [[ $1 == "full" ]]; then
 	kubectl delete -f highload-deployment.yaml
 	kubectl delete -f highload-services.yaml
 	kubectl delete -f highload-autoscaler.yaml
+	kubectl delete -f highload-service-monitor.yaml
 fi
