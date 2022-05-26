a040bb52516c9401cd7a6cfce2834148c5e13e85
Ubuntu <ubuntu@ip-172-31-42-68.us-east-2.compute.internal>
2022-05-26 14:15:01 +0000

    load test script changes

--- xk6/script.js
+++ xk6/script.js
@@ -2,28 +2,55 @@ import { check } from 'k6';
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+var prealloc = {
+	highload: 3000, // 11000,
+	highmem:  1500, // 300,
+	highcpu:  1500, // 300,
+	lowload:  1500, // 300
+};
+var highcpuCount = 200;
+var highmemCount = 20;
+
 
 export const options = {
   scenarios: {
     highload: {
       executor: 'ramping-arrival-rate',
       exec: 'highload',
-      preAllocatedVUs: 11000,
+      preAllocatedVUs: prealloc.highload,
       stages: [
         { duration: '1m', target: 300 },
-        { duration: '1m', target: 900 },
-        { duration: '30s', target: 10000 },
-        { duration: '1m', target: 1000 },
-        { duration: '1m', target: 0 }
+        { duration: '3m', target: prealloc.highload },
+        { duration: '30s', target: 0 }
       ]    
+    },
+    highmem: {
+      executor: 'ramping-arrival-rate',
+      exec: 'highmem',
+      preAllocatedVUs: prealloc.highmem,
+      stages: [
+        { duration: '1m', target: 300 },
+        { duration: '3m', target: prealloc.highmem },
+        { duration: '30s', target: 0 }
+      ]
+    },
+    highcpu: {
+      executor: 'ramping-arrival-rate',
+      exec: 'highcpu',
+      preAllocatedVUs: prealloc.highcpu,
+      stages: [
+        { duration: '1m', target: 300 },
+        { duration: '3m', target: prealloc.highcpu },
+        { duration: '30s', target: 0 }
+      ]
     },
     lowload: {
       executor: 'ramping-arrival-rate',
       exec: 'lowload',
-      preAllocatedVUs: 300,
+      preAllocatedVUs: prealloc.lowload,
       stages: [
         { duration: '1m', target: 300 },
-        { duration: '3m', target: 300 },
+        { duration: '3m', target: prealloc.lowload },
         { duration: '30s', target: 0 }
       ]
     }
@@ -43,13 +70,31 @@ export const options = {
   },
 };
 
-const hostname = 'aa7db044357554675bdf584d6d1fdffc-1919935816.us-east-2.elb.amazonaws.com';
+const hostname = 'a3ae3226c4e37450ca10ff855f2fed15-453678147.us-east-2.elb.amazonaws.com';
 
 export function highload () {
   const res = http.get('http://'+hostname+'/highload');
   check(res, {
     'verify homepage text': (r) =>
       r.body.includes('highload'),
+  });
+  sleep(1);
+}
+
+export function highcpu() {
+  const res = http.get('http://'+hostname+'/highcpu?count='+highcpuCount);
+  check(res, {
+    'verify homepage text': (r) =>
+      r.body.includes('highcpu'),
+  });
+  sleep(1);
+}
+
+export function highmem() {
+  const res = http.get('http://'+hostname+'/highmem?count='+highmemCount);
+  check(res, {
+    'verify homepage text': (r) =>
+      r.body.includes('highmem'),
   });
   sleep(1);
 }
