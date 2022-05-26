7fd9616cd497eaaece50678d825484f0101a3fe2
Ubuntu <ubuntu@ip-172-31-42-68.us-east-2.compute.internal>
2022-05-26 08:58:39 +0000

    ubuntu run scripts.

--- xk6/run.sh
+++ xk6/run.sh
@@ -1,2 +1,3 @@
+ulimit -n 65536
 K6_PROMETHEUS_REMOTE_URL=http://localhost:9090/api/v1/write \
 ./k6 run script.js -o output-prometheus-remote
--- xk6/script.js
+++ xk6/script.js
@@ -2,32 +2,29 @@ import { check } from 'k6';
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+
 export const options = {
   scenarios: {
     info1: {
+      executor: 'ramping-arrival-rate',
+      exec: 'info1',
+      preAllocatedVUs: 11000,
       stages: [
         { duration: '1m', target: 300 },
-        { duration: '2m', target: 900 },
-        { duration: '2m', target: 1500 },
-        { duration: '2m', target: 2000 },
-        { duration: '3m', target: 2300 },
-        { duration: '6m', target: 2000 },
-        { duration: '2m', target: 1000 },
-        { duration: '1m', target: 500 },
+        { duration: '1m', target: 900 },
+        { duration: '30s', target: 10000 },
+        { duration: '1m', target: 1000 },
         { duration: '1m', target: 0 }
       ]    
     },
     info2: {
+      executor: 'ramping-arrival-rate',
+      exec: 'info2',
+      preAllocatedVUs: 300,
       stages: [
         { duration: '1m', target: 300 },
-        { duration: '2m', target: 900 },
-        { duration: '2m', target: 1500 },
-        { duration: '2m', target: 2000 },
-        { duration: '3m', target: 2300 },
-        { duration: '6m', target: 2000 },
-        { duration: '2m', target: 1000 },
-        { duration: '1m', target: 500 },
-        { duration: '1m', target: 0 }
+        { duration: '3m', target: 300 },
+        { duration: '30s', target: 0 }
       ]
     }
   },
@@ -46,8 +43,10 @@ export const options = {
   },
 };
 
+const hostname = 'aa7db044357554675bdf584d6d1fdffc-1919935816.us-east-2.elb.amazonaws.com';
+
 export function info1 () {
-  const res = http.get('http://a1268da6f6ffb4780b47832ec41d452c-1058680272.us-east-2.elb.amazonaws.com/info1');
+  const res = http.get('http://'+hostname+'/info1');
   check(res, {
     'verify homepage text': (r) =>
       r.body.includes('info1'),
@@ -55,12 +54,13 @@ export function info1 () {
   sleep(1);
 }
 
-export function info1 () {
-  const res = http.get('http://a1268da6f6ffb4780b47832ec41d452c-1058680272.us-east-2.elb.amazonaws.com/info2');
+export function info2 () {
+  const res = http.get('http://'+hostname+'/info2');
   check(res, {
     'verify homepage text': (r) =>
       r.body.includes('info2'),
   });
   sleep(1);
 }
+
 
