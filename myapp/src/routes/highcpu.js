a419c6ecba2f7844159667f1b1796af6fc52e26e
Shivam Nagar <shivam.nagar@outlook.com>
2022-06-01 14:34:24 +0530

    Added sum of fibs to highcpu API.

--- myapp/src/routes/highcpu.js
+++ myapp/src/routes/highcpu.js
@@ -14,10 +14,26 @@ function hackySack(dummyData, recCount) {
     return newData;
 }
 
+function fibonacci(nth) {
+    var num1 = 1;
+    var num2 = 1;
+    var temp;
+    for(var i=0; i<nth; i++) {
+        temp = num2;
+        num2 = num1 + num2;
+        num1 = temp;
+    }
+    return num2;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {    
     var recCount = req.query.count || 10;
-    var finalData = hackySack(jsondata, recCount);
+    var finalData = 1; // hackySack(jsondata, recCount);
+    
+    for(var i=0; i<recCount; i++) {
+        finalData += fibonacci(i);
+    }
 
     let ts = Date.now();
     let date = new Date(ts);
@@ -25,7 +41,7 @@ router.get('/', function(req, res, next) {
     highcpuCounter.inc({ code: 200 });
     res.send({ 
         api: 'highcpu',
-        count: finalData.depth,
+        sum_of_fibs: finalData,
         date
     });
 });
