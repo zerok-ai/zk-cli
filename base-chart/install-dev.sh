bd0d15c67531dc991a7643f62e54591ffcdc44d7
avingoyal <avingoyal01+github@gmail.com>
2023-08-23 12:59:29 +0530

    separating out dev install

---
+++ base-chart/install-dev.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+#
+#if [ "$#" -eq "0" ]; then
+#  echo "Invalid cli arguments. ERR #1"
+#  exit 1
+#fi
+#
+#while [[ "$#" > "0" ]]
+#do
+#  case $1 in
+#    (*=*) eval $1;;
+#  esac
+#shift
+#done
+#
+#if [ -z "$PX_API_KEY" ] || [ -z "$PX_CLUSTER_KEY" ] || [ -z "$ZK_CLOUD_ADDR" ]
+#then
+#  echo "Invalid cli arguments. ERR #2"
+#  exit 1
+#fi
+
+echo "checking helm binary"
+if ! helm version; then
+    echo "helm not available. ERR #4"
+    exit 1
+else
+    echo "helm binary found."
+fi
+
+helm repo add zk-scenario-manager https://helm.zerok.ai/zk-client/zk-scenario-manager
+helm repo update
+helm upgrade zk-scenario-manager zk-scenario-manager/zk-scenario-manager --install --create-namespace --namespace zk-client --version 0.0.1-dev
+
+helm repo add zk-axon https://helm.zerok.ai/zk-client/zk-axon
+helm repo update
+helm upgrade zk-axon zk-axon/zk-axon --install --create-namespace --namespace zk-client --version 0.0.1-dev
---
+++ base-chart/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ "$#" -eq "0" ]; then
+  echo "Invalid cli arguments. ERR #1"
+  exit 1
+fi
+
+while [[ "$#" > "0" ]]
+do
+  case $1 in
+    (*=*) eval $1;;
+  esac
+shift
+done
+
+if [ -z "$PX_API_KEY" ] || [ -z "$PX_CLUSTER_KEY" ] || [ -z "$ZK_CLOUD_ADDR" ]
+then
+  echo "Invalid cli arguments. ERR #2"
+  exit 1
+fi
+
+echo "checking helm binary"
+if ! helm version; then
+    echo "helm not available. ERR #4"
+    exit 1
+else
+    echo "helm binary found."
+fi
+
+helm dependency build $THIS_DIR
+helm dependency update $THIS_DIR
+helm --install --set=zk-axon.releaseName=zk-axon --set=zk-scenario-manager.releaseName=zk-scenario-manager --set=global.vizier.tag=$VIZIER_TAG --set=global.zkcloud.host=$ZK_CLOUD_ADDR --set=global.data.cluster_key=$PX_CLUSTER_KEY --set=global.data.PX_API_KEY=$PX_API_KEY upgrade $APP_NAME $THIS_DIR/ --create-namespace --namespace zk-client
