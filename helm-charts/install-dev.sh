70741a3b1bf38d57776408ad8c95c8e81cb6f73d
avingoyal <avingoyal01+github@gmail.com>
2023-09-06 00:31:39 +0530

    fixing issue with app-init-container

--- helm-charts/install-dev.sh
+++ helm-charts/install-dev.sh
@@ -40,19 +40,19 @@ helm repo add zk-axon https://privatehelm.zerok.ai/zk-client/zk-axon
 helm repo add zk-daemonset https://privatehelm.zerok.ai/zk-client/zk-daemonset
 helm repo add zk-wsp-client https://privatehelm.zerok.ai/zk-client/zk-wsp-client
 helm repo add zk-operator https://privatehelm.zerok.ai/zk-client/zk-operator
-helm repo add zk-app-init-containers https://privatehelm.zerok.ai/zk-client/zk-app-init-containers
+helm repo add zk-app-init-container https://privatehelm.zerok.ai/zk-client/zk-app-init-container
 #helm repo add zk-gpt https://privatehelm.zerok.ai/zk-client/zk-gpt
 
 # update
 helm repo update
 
 # install
-helm upgrade zk-app-init-containers zk-app-init-containers/zk-app-init-containers --install --create-namespace --namespace zk-client --version $ZK_APP_INIT_CONTAINERS_VERSION
+helm upgrade zk-app-init-container zk-app-init-container/zk-app-init-container --install --create-namespace --namespace zk-client --version $ZK_APP_INIT_CONTAINERS_VERSION
 helm upgrade zk-wsp-client zk-wsp-client/zk-wsp-client --install --create-namespace --namespace zk-client --version $ZK_WSP_CLIENT_VERSION --set=global.zkcloud.host=$ZK_CLOUD_ADDR
 helm upgrade zk-operator zk-operator/zk-operator --install --create-namespace --namespace zk-client --version $ZK_OPERATOR_VERSION
 helm upgrade zk-scenario-manager zk-scenario-manager/zk-scenario-manager --install --create-namespace --namespace zk-client --version $ZK_SCENARIO_MANAGER_VERSION
 helm upgrade zk-axon zk-axon/zk-axon --install --create-namespace --namespace zk-client --version $ZK_AXON_VERSION
 helm upgrade zk-daemonset zk-daemonset/zk-daemonset --install --create-namespace --namespace zk-client --version $ZK_DAEMONSET_VERSION
-#helm upgrade zk-gpt zk-gpt/zk-gpt --install --create-namespace --namespace zk-client --version $ZK_GPT_VERSION
+helm upgrade zk-gpt zk-gpt/zk-gpt --install --create-namespace --namespace zk-client --version $ZK_GPT_VERSION
 
 
